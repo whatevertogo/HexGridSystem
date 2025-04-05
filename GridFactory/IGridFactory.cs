a4db22fe4967e62f64f8a60847b51199b439ef99
whatevertogo <1879483647@qq.com>
2025-04-06 01:28:12 +0800

    删除未使用的网格和接口文件

--- GridFactory/IGridFactory.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using Core.Interfaces;
-
-public interface IGridFactory
-{
-    public HexCell CreateGridCell(Vector3 position, ITerrainCell terrain, IDiceContainer dice, ICivilizationTerritory territory);
-
-}
